1784005f5c401b079b7fae69f76a8816c870e7b0
BETUANMINH <minhbthe176084@fpt.edu.vn>
2023-10-22 17:55:25 +0700

    view order

---
+++ Controllers/OrderProcess.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+//impot linq
+using System.Linq;
+namespace BookShoppingCartMVC.Controllers
+{
+    public class OrderProcess : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderProcess(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            
+
+            //select o.Id, u.UserName, o.CreateDate, o.OrderStatusId, o.IsDeleted from Order_HE176084 o join AspNetUsers_HE176084 u on o.UserID = u.Id
+            var orders = (from o in _context.Orders
+                          join u in _context.Users on o.UserID equals u.Id
+                          join os in _context.OrderStatus on o.OrderStatusId equals os.Id
+                         select new OrderUserView
+                                                                            {
+                              Id = o.Id,
+                              UserName = u.UserName,
+                              CreateDate = o.CreateDate,
+                              StatusName = os.StatusName,
+                              IsDeleted = o.IsDeleted
+                          }).ToList();
+            ViewBag.Orders = orders;
+            return View();
+        }
+        //View Order Details
+        public IActionResult Details(int id)
+        {
+            var rs = _context.OrderDetails.Where(x => x.OrderId == id)
+                .Include(x => x.Book)
+                .ThenInclude(x => x.Genre)
+                .ToList();
+            ViewBag.OrderId = id;
+            return View(rs);
+        }   
+    }
+}
---
+++ Models/DTOs/OrderUserView.cs
@@ -0,0 +1,12 @@
+﻿namespace BookShoppingCartMVC.Models.DTOs
+{
+    public class OrderUserView
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreateDate { get; set; }
+        public String StatusName { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+}
